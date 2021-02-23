5cc7663d10c7c309a03a6f5bad4f332cbaee5307
Norbel Ambanumben <aanorbel@gmail.com>
2021-02-23 20:36:22 +0100

    modified readme and removed code duplication

--- lib/src/main/java/net/compay/android/models/requests/WithdrawRequest.java
+++ lib/src/main/java/net/compay/android/models/requests/WithdrawRequest.java
@@ -1,95 +1,11 @@
 package net.compay.android.models.requests;
 
-import com.google.gson.annotations.SerializedName;
+import net.compay.android.models.commons.Withdrawal;
 
 /**
  * The type Withdraw request.
  */
-public class WithdrawRequest {
-    private String amount;
-    private String to;
-    private String description;
-    @SerializedName("external_reference")
-    private String externalReference;
-
-    /**
-     * Instantiates a new Withdraw request.
-     */
-    public WithdrawRequest() {
-    }
-
-    /**
-     * Gets amount.
-     *
-     * @return the amount
-     */
-    public String getAmount() {
-        return amount;
-    }
-
-    /**
-     * Sets amount.
-     *
-     * @param amount the amount
-     */
-    public void setAmount(String amount) {
-        this.amount = amount;
-    }
-
-    /**
-     * Gets to.
-     *
-     * @return the to
-     */
-    public String getTo() {
-        return to;
-    }
-
-    /**
-     * Sets to.
-     *
-     * @param to the to
-     */
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    /**
-     * Gets description.
-     *
-     * @return the description
-     */
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * Sets description.
-     *
-     * @param description the description
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    /**
-     * Gets external reference.
-     *
-     * @return the external reference
-     */
-    public String getExternalReference() {
-        return externalReference;
-    }
-
-    /**
-     * Sets external reference.
-     *
-     * @param externalReference the external reference
-     */
-    public void setExternalReference(String externalReference) {
-        this.externalReference = externalReference;
-    }
-
+public class WithdrawRequest extends Withdrawal {
 
     /**
      * The type Withdraw request builder.
