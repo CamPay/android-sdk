32d9376e3e95a0bcc61f70037772448e9b659356
Norbel Ambanumben <aanorbel@gmail.com>
2021-02-23 20:52:54 +0100

    added tests stub

---
+++ lib/src/androidTest/java/net/compay/android/CamPayTest.java
@@ -0,0 +1,34 @@
+package net.compay.android;
+
+import junit.framework.TestCase;
+
+public class CamPayTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        throw UnsupportedOperationException();
+    }
+
+    public void testCollect() {
+        throw UnsupportedOperationException();
+
+    }
+
+    public void testTransactionStatus() {
+        throw UnsupportedOperationException();
+
+    }
+
+    public void testWithdraw() {
+        throw UnsupportedOperationException();
+
+    }
+
+    public void testApplicationBalance() {
+        throw UnsupportedOperationException();
+
+    }
+}
